1847cd44b4cf9a5b4e73b28a9c0d1fef2d6cef2f
ronin1991 <ronin1991@rambler.ru>
2020-01-04 09:47:39 +0200

    createGreeting

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env  node
 "use strict";
 
-console.log("Welcome to the Brain Games!");
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw2QkFBWiIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52ICBub2RlXG5jb25zb2xlLmxvZyhcIldlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzIVwiKTtcblxuIl19
+var _ = require("..");
+
+console.log("Welcome to the Brain Games!\n");
+(0, _.greetingUser)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUVBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSwrQkFBWjtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgIG5vZGVcbmltcG9ydCB7Z3JlZXRpbmdVc2VyfSBmcm9tICcuLic7XG5cbmNvbnNvbGUubG9nKFwiV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhXFxuXCIpO1xuZ3JlZXRpbmdVc2VyKCk7XG5cbiJdfQ==
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env  node
-console.log("Welcome to the Brain Games!");
+import {greetingUser} from '..';
+
+console.log("Welcome to the Brain Games!\n");
+greetingUser();
 
