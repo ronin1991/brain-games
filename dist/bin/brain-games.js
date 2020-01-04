9ad71b79c6ddbe192900ba7c25acc846681b2972
ronin1991 <ronin1991@rambler.ru>
2020-01-04 22:19:35 +0200

    update-travis-3

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env  node
 "use strict";
 
-var _ = require("..");
+var _ = _interopRequireDefault(require(".."));
 
-console.log("Welcome to the Brain Games!\n");
-(0, _.greetingUser)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUVBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSwrQkFBWjtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgIG5vZGVcbmltcG9ydCB7Z3JlZXRpbmdVc2VyfSBmcm9tICcuLic7XG5cbmNvbnNvbGUubG9nKFwiV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhXFxuXCIpO1xuZ3JlZXRpbmdVc2VyKCk7XG5cbiJdfQ==
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('Welcome to the Brain Games!\n');
+(0, _.default)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOzs7O0FBRUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLCtCQUFaO0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiAgbm9kZVxuaW1wb3J0IGdyZWV0aW5nVXNlciBmcm9tICcuLic7XG5cbmNvbnNvbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyFcXG4nKTtcbmdyZWV0aW5nVXNlcigpO1xuIl19
