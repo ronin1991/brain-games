b43a81d4e1d2340ef22b1316622a8c05df247bf8
ronin1991 <ronin1991@rambler.ru>
2020-01-16 17:47:34 +0200

    update ignore two

--- dist/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env  node
-"use strict";
-
-var _ = require("..");
-
-(0, _.greetingUser)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUVBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgIG5vZGVcbmltcG9ydCB7IGdyZWV0aW5nVXNlciB9IGZyb20gJy4uJztcblxuZ3JlZXRpbmdVc2VyKCk7XG4iXX0=
